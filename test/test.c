d016c9c53bb98f72e10cbb8e69310116cd5b3e3a
Fineas <finymelly@gmail.com>
2020-11-07 16:06:40 +0200

    test3

--- test/test.c
+++ test/test.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(){
-    system("ls -l");
     puts("FeDEX");
+    int ret;
+    char *cmd[] = { "ls", "-l", (char *)0 };
+    char *env[] = {};
+    ret = execve ("/bin/ls", cmd, env);
+
 }
 
 // https://fineas.github.io/FeDEX/test/a.out
